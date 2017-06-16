1ebb68149fb72271fdbe5ddbc015cd4d57420c7f
Goran Minov <goran.s.minov@gmail.com>
2017-06-16 23:48:01 +0100

    Added more jokes

--- jokelibrary/src/main/java/com/example/JokeTeller.java
+++ jokelibrary/src/main/java/com/example/JokeTeller.java
@@ -5,9 +5,13 @@ import java.util.Random;
 public class JokeTeller {
 
     private String[] jokes = {
-            "What do you call a programmer from Finland? -Nerdic.",
+            "What do you call a programmer from Finland? -Nerdic",
             "What is a programmer's favourite hangout place? -Foo Bar",
-            "Why did the programmer quit his job? -Because he didn't get arrays."
+            "Why did the programmer quit his job? -Because he didn't get arrays",
+            "0 is false and 1 is true, right? -1",
+            "Why do Java programmers have to wear glasses? -Because they don't see C#",
+            "Real programmers count from 0",
+            "Chuck Norris can take a screenshot of his blue screen"
     };
 
     private Random random = new Random();
