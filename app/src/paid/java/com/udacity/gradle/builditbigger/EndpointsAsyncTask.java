c2d2a21c0520c13f25562cc41b91e4b8d04d462c
Goran Minov <goran.s.minov@gmail.com>
2017-06-16 22:11:28 +0100

    Added paid flavor

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -0,0 +1,55 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Pair;
+
+import com.example.goranminov.myapplication.backend.myApi.MyApi;
+import com.example.goranminov.myapplication.backend.myApi.model.MyBean;
+import com.example.jokedisplay.JokeDisplay;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+
+import java.io.IOException;
+
+/**
+ * Created by goranminov on 11/06/2017.
+ */
+
+class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    public EndpointsAsyncTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Pair<Context, String>... params) {
+        if (myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("https://testjokes-170420.appspot.com/_ah/api/");
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        try {
+            return myApiService.newJoke(new MyBean()).execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Intent intent = new Intent(context, JokeDisplay.class);
+        intent.putExtra(JokeDisplay.INTENT_EXTRA, result);
+        context.startActivity(intent);
+    }
+}
